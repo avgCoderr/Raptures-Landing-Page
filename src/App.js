29651771d3637e56c2a917560bfeb7f6e62d4c91
Rishabh <f20201677@pilani.bits-pilani.ac.in>
2023-08-27 14:28:19 +0530

    small changes to html

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import "./App.css";
 import Background from "./components/Background";
 import NavBar from "./components/Navbar";
 import Home from "./components/Home";
-import Features from "./components/Features";
 
 const App = () => {
   return (
