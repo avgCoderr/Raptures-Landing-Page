9b079fd854b81bd485a3d2f1bd096f5a082e5a8b
Rishabh <f20201677@pilani.bits-pilani.ac.in>
2023-08-28 16:03:39 +0530

    UI changes: font change, basic responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,59 @@
 import React from "react";
-import { Box, Typography, Button } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  ThemeProvider,
+  Container,
+  Grid,
+  useMediaQuery,
+} from "@mui/material";
+import { createTheme, useTheme } from "@mui/material/styles";
 
 import NotStartedOutlinedIcon from "@mui/icons-material/NotStartedOutlined";
 import VideocamIcon from "@mui/icons-material/Videocam";
 
 import image from "../assets/discord-bot.png";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Manrope', sans-serif",
+  },
+});
+
 const HomeScreen = () => {
   const parentBoxStyle = {
+    marginTop: "2rem",
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    height: "100vh",
-    margin: "0rem 5rem 0rem 4rem",
+    minHeight: "100vh",
     position: "relative",
   };
 
+  const currentTheme = useTheme();
+  const isMobileView = useMediaQuery(currentTheme.breakpoints.down("sm"));
+
+  const titleFontSize = isMobileView ? "2rem" : "3rem";
+  const descriptionFontSize = isMobileView ? "1.2rem" : "1.5rem";
+
   const contentStyle = {
-    padding: "4rem",
-    paddingRight: "8rem",
+    padding: isMobileView ? "1rem" : "2rem",
     color: "white",
   };
 
   const titleStyle = {
     marginBottom: "1rem",
-    fontSize: "3rem",
+    fontSize: titleFontSize,
     fontWeight: "bold",
     color: "white",
   };
 
   const descriptionStyle = {
-    fontSize: "1.5rem",
+    fontSize: descriptionFontSize,
     fontWeight: 50,
-    marginBottom: "3rem",
+    marginBottom: "2rem",
   };
 
   const buttonsContainerStyle = {
@@ -43,38 +63,61 @@ const HomeScreen = () => {
   };
 
   return (
-    <Box>
-      <Box sx={parentBoxStyle}>
-        <Box sx={contentStyle}>
-          <Typography variant="h1" sx={titleStyle}>
-            Forget what you knew about Web3 wallets.
-          </Typography>
-          <Typography variant="body1" sx={descriptionStyle}>
-            Convert your social media accounts into gateways to the Web3 world. 
-          </Typography>
-          <Typography variant="body1" sx={descriptionStyle}>
-            Now available on Discord
-          </Typography>
-          <Box sx={buttonsContainerStyle}>
-            <Button
-              variant="contained"
-              color="primary"
-              startIcon={<NotStartedOutlinedIcon />}
-            >
-              START
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              startIcon={<VideocamIcon />}
-            >
-              DEMO
-            </Button>
-          </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Box sx={parentBoxStyle}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {/* Content on the left, 6 columns for large screens */}
+              <Grid item xs={12} lg={6}>
+                <Box sx={contentStyle}>
+                  <Typography variant="h1" sx={titleStyle}>
+                    Forget what you knew about Web3 wallets.
+                  </Typography>
+                  <Typography variant="body2" sx={descriptionStyle}>
+                    Convert your social media accounts into gateways to the Web3
+                    world.
+                  </Typography>
+                  <Typography variant="body2" sx={descriptionStyle}>
+                    Now available on Discord!
+                  </Typography>
+                  <Box sx={buttonsContainerStyle}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      startIcon={<NotStartedOutlinedIcon />}
+                      sx={{ paddingRight: "3rem", paddingLeft: "3rem" }}
+                    >
+                      START
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      startIcon={<VideocamIcon />}
+                      sx={{ paddingRight: "3rem", paddingLeft: "3rem" }}
+                    >
+                      DEMO
+                    </Button>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <img
+                  src={image}
+                  alt="Logo"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    marginTop: "2rem",
+                    marginBottom: "4rem",
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Container>
         </Box>
-        <img src={image} alt="Logo" style={{ height: "auto" }} />
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-import { AppBar, Toolbar, Link, Box, Button } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Link,
+  Box,
+  ThemeProvider,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import logo from "../assets/logo.svg";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Manrope', sans-serif",
+  },
+});
 
 const NavBar = () => {
   const appBarStyle = {
@@ -18,42 +38,116 @@ const NavBar = () => {
     marginLeft: "2rem",
   };
 
-  const linkStyle = { marginLeft: 8, color: "inherit", textDecoration: "none" };
-  const buttonStyle = {
+  const linkStyle = {
     marginLeft: 8,
     color: "inherit",
-    padding: "0.6rem 2.2rem 0.6rem 2.2rem",
-    borderRadius: "5px",
-    background:
-      "var(--glass, radial-gradient(151.92% 127.02% at 15.32% 10%, rgba(165, 239, 255, 0.20) 0%, rgba(110, 191, 244, 0.04) 100%, rgba(70, 144, 212, 0.00) 0%))",
+    textDecoration: "none",
+  };
+
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
   };
 
   return (
-    <AppBar position="fixed" sx={appBarStyle}>
-      <Toolbar>
-        <Link href="/home" color="inherit">
-          <img src={logo} alt="Logo" style={logoStyle} />
-        </Link>
-        <Box sx={{ marginLeft: "auto", marginRight: "4rem" }}>
-          <Link href="/docs" color="inherit" sx={linkStyle}>
-            Docs
+    <ThemeProvider theme={theme}>
+      <AppBar position="fixed" sx={appBarStyle}>
+        <Toolbar>
+          <Link href="/home" color="inherit">
+            <img src={logo} alt="Logo" style={logoStyle} />
           </Link>
-          <Link href="/features" color="inherit" sx={linkStyle}>
-            Features
-          </Link>
-          <Link href="/blog" color="inherit" sx={linkStyle}>
-            Blog
-          </Link>
-          <Link href="/pricing" color="inherit" sx={linkStyle}>
-            Pricing
-          </Link>
-          <Link href="/company" color="inherit" sx={linkStyle}>
-            Company
-          </Link>
-          <Button sx={buttonStyle}>Start</Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+          <IconButton
+            sx={{ display: { sm: "block", md: "none" } }}
+            onClick={toggleMobileMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            anchor="left"
+            open={mobileMenuOpen}
+            onClose={toggleMobileMenu}
+            sx={{ background: "transparent" }}
+          >
+            <Box p={2}>
+              <Typography variant="h6">Menu</Typography>
+            </Box>
+            <List>
+              <ListItem>
+                <Link
+                  href="/docs"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Docs" />
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href="/features"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Features" />
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href="/blog"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Blog" />
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href="/pricing"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Pricing" />
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  href="/company"
+                  color="inherit"
+                  sx={{ textDecoration: "none" }}
+                >
+                  <ListItemText primary="Company" />
+                </Link>
+              </ListItem>
+            </List>
+          </Drawer>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+              marginLeft: "auto",
+              marginRight: "4rem",
+              flexDirection: "row",
+            }}
+          >
+            <Link href="/docs" color="inherit" sx={linkStyle}>
+              Docs
+            </Link>
+            <Link href="/features" color="inherit" sx={linkStyle}>
+              Features
+            </Link>
+            <Link href="/blog" color="inherit" sx={linkStyle}>
+              Blog
+            </Link>
+            <Link href="/pricing" color="inherit" sx={linkStyle}>
+              Pricing
+            </Link>
+            <Link href="/company" color="inherit" sx={linkStyle}>
+              Company
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
